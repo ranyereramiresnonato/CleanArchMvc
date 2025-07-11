8ec43f2b95fb23eb926e25a2e049d29a5e80570f
ranyereramiresdeveloper <ranyereramiresdeveloper@hotmail.com>
2025-07-11 15:31:32 -0300

    Iniciando a integração do auto mapper

---
+++ CleanArchMcv.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArchMcv.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMcv.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
